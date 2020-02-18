f75d96b43d73c7971e2c29c423087745d6094ccf
Ari Katz <arikatz.d@gmail.com>
2020-02-17 20:39:58 -0800

    blend

---
+++ script.js
@@ -0,0 +1,93 @@
+class Problem {
+  constructor(type, vars) {
+    this.type = type;
+    this.vars = vars;
+  }
+}
+
+var processbase = {
+  "third.multiply.simple.twovar": {
+    1: "First, we look at our two numbers ``..a..`` and ``..b..``.",
+    2: "Then, we have ``..a..`` ÷ ``..b..``.",
+    3: "Who knew I loved step 3!"
+  }
+};
+
+let exampleproblem = new Problem("third.multiply.simple.twovar", {
+  a: Math.round(Math.random() * 10),
+  b: Math.round(Math.random() * 10)
+});
+
+function parseSolveObject(problem) {
+  let output = {};
+  for (let w = 0; w < Object.keys(processbase[problem.type]).length; w++) {
+    var parse = processbase[problem.type][w + 1].split("``");
+    for (let i = 0; i < parse.length; i++) {
+      if (parse[i][0] == "~" && problem.vars[parse[i].slice(1)]) {
+        parse[i] = `~[${problem.vars[parse[i].slice(1)]}]`;
+      }
+    }
+    output[w + 1] = parse; //.join('');
+  }
+  return output;
+}
+
+function makeDisplayArray(problem) {
+  var steps = parseSolveObject(problem);
+  var displayArray = [];
+
+  for (let i = 1; i < Object.keys(steps).length + 1; i++) {
+    let tempPart = [];
+    for (let j = 0; j < Object.keys(steps[i]).length; j++) {
+      let tempobj = { content: "", color: "" };
+      var text = steps[i][j];
+      if (steps[i][j][0] == "." && steps[i][j][1] == ".") {
+        //isASpecialType
+        text = text.slice(2);
+        text = text.substring(0, text.length - 2);
+        if (text.length == 1) {
+          //isASingleVariable
+          var varReplace = problem.vars[text]; //put actual number in place for single letter var
+          tempPart.push({
+            string: varReplace,
+            type: "var-" + text.toUpperCase()
+          });
+        } else {
+          //TODO: process multi variable block with operators
+          tempPart.push({
+            string: text,
+            type: "multiTODO"
+          });
+        }
+      } else {
+        tempPart.push({
+          string: text,
+          type: "text"
+        });
+      }
+    }
+    displayArray.push(tempPart);
+  }
+  return displayArray;
+}
+
+function printSteps(problem) {
+  var displayArray = makeDisplayArray(problem);
+  var innerHtmlString = "<br/>";
+  console.log(innerHtmlString);
+  for (let i = 0; i < displayArray.length; i++) {
+    innerHtmlString += `Step ${i + 1}:<br/><div class="text">`;
+    for (let j = 0; j < displayArray[i].length; j++) {
+      if (displayArray[i][j].type == "text") {
+        innerHtmlString += displayArray[i][j].string;
+      } else {
+        innerHtmlString += `<span class=${displayArray[i][j].type}>${displayArray[i][j].string}</span>`;
+      }
+    }
+    innerHtmlString += `</div><br/>`;
+  }
+  document.getElementById("jj").innerHTML = innerHtmlString;
+}
+console.log(makeDisplayArray(exampleproblem));
+
+printSteps(exampleproblem);
