2b6fa0c28bcc0c89e385950f0ab659a3b86f4673
Ari Katz <arikatz.d@gmail.com>
2020-02-18 05:44:36 -0800

    fixer

--- script.js
+++ script.js
@@ -5,28 +5,37 @@ class Problem {
   }
 }
 
+var firstv = Math.round(Math.random() * 12);
+var secondv = Math.round(Math.random() * 12);
+
+document.getElementById("var1").innerHTML = firstv;
+document.getElementById("var2").innerHTML = secondv;
+
+let exampleproblem = new Problem("another.example.for.you", {
+  a: firstv,
+  b: secondv
+});
+
 var processbase = {
+  //grade.type.subtype.subtype...subtype
   "third.multiply.simple.twovar": {
-    1: "First, we look at our two numbers ``..a..`` and ``..b..``.",
-    2: "Then, we have ``..a..`` ÷ ``..b..``.",
-    3: "Who knew I loved step 3!"
+    1: "First, we look at our two numbers ``..a`` and ``..b``.",
+    2: "Then, we have ``..a`` ÷ ``..b``.",
+    3: "Who knew I loved step 3!",
+    4: "GOLBDY GOOK BOOK ``..b``"
+  },
+  "another.example.for.you": {
+    1: "``..a`` and ``..b``, what great numbers aren't they?",
+    2: "I honestly can't choose between ``..a`` and ``..b``.",
+    3: "Who knew I loved step 3!",
+    4: "GOLBDY GOOK BOOK ``..b``"
   }
 };
-
-let exampleproblem = new Problem("third.multiply.simple.twovar", {
-  a: Math.round(Math.random() * 10),
-  b: Math.round(Math.random() * 10)
-});
 
 function parseSolveObject(problem) {
   let output = {};
   for (let w = 0; w < Object.keys(processbase[problem.type]).length; w++) {
     var parse = processbase[problem.type][w + 1].split("``");
-    for (let i = 0; i < parse.length; i++) {
-      if (parse[i][0] == "~" && problem.vars[parse[i].slice(1)]) {
-        parse[i] = `~[${problem.vars[parse[i].slice(1)]}]`;
-      }
-    }
     output[w + 1] = parse; //.join('');
   }
   return output;
@@ -44,7 +53,6 @@ function makeDisplayArray(problem) {
       if (steps[i][j][0] == "." && steps[i][j][1] == ".") {
         //isASpecialType
         text = text.slice(2);
-        text = text.substring(0, text.length - 2);
         if (text.length == 1) {
           //isASingleVariable
           var varReplace = problem.vars[text]; //put actual number in place for single letter var
@@ -76,7 +84,7 @@ function printSteps(problem) {
   var innerHtmlString = "<br/>";
   console.log(innerHtmlString);
   for (let i = 0; i < displayArray.length; i++) {
-    innerHtmlString += `Step ${i + 1}:<br/><div class="text">`;
+    innerHtmlString += `<div class="text"> ${i + 1}. `;
     for (let j = 0; j < displayArray[i].length; j++) {
       if (displayArray[i][j].type == "text") {
         innerHtmlString += displayArray[i][j].string;
@@ -86,7 +94,9 @@ function printSteps(problem) {
     }
     innerHtmlString += `</div><br/>`;
   }
-  document.getElementById("jj").innerHTML = innerHtmlString;
+  document.getElementsByClassName(
+    "student-quiz-explanationcontainer"
+  )[0].innerHTML = innerHtmlString;
 }
 console.log(makeDisplayArray(exampleproblem));
 
